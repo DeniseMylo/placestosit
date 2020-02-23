54dc4875aa6a7e10cd654a79119f86fcb5d15b2f
philbeaudry <philippe.beaudry96@gmail.com>
2020-02-23 17:56:57 -0500

    small fixes to the scroll

--- TootsieRoll/script.js
+++ TootsieRoll/script.js
@@ -1,34 +1,45 @@
 
 $(window).scroll(function() {
    var value = $(this).scrollTop();
-   if (value > 2700){
+   if (value > 5900) {
     $(".artwork").attr("src", "./image/wrapper-3.png");
+    document.getElementById("roll").style.display = 'block'
    } 
-   else if (value > 2400){
+   else if (value > 5200){
     $(".artwork").attr("src", "./image/wrapper-2.png");
+    document.getElementById("roll").style.display = 'block'
    } 
-   else if (value > 2100){
+   else if (value > 4600){
     $(".artwork").attr("src", "./image/wrapper-1.png");
+    document.getElementById("roll").style.display = 'block'
    } 
-   else if (value > 1800){
+   else if (value > 3900){
     $(".artwork").attr("src", "./image/Artwork-7.png");
+    document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 1500){
+   else if (value > 3200){
     $(".artwork").attr("src", "./image/Artwork-6.png");
+    document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 1200){
+   else if (value > 2700){
     $(".artwork").attr("src", "./image/Artwork-5.png");
+    document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 900){
+   else if (value > 2000){
     $(".artwork").attr("src", "./image/Artwork-4.png");
+    document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 600){
+   else if (value > 1400){
     $(".artwork").attr("src", "./image/Artwork-3.png");
+    document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 300){
+   else if (value > 700){
     $(".artwork").attr("src", "./image/Artwork-2.png");
+    document.getElementById("roll").style.display = 'none'
    }   
-   else
+   else{
       $(".artwork").attr("src", "./image/Artwork-1.png");
+      document.getElementById("roll").style.display = 'none'
+   }
 });
 
