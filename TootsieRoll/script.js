e32ab4c69d9edf9fe5d58e3d1019876d0f1da5da
Phil Beaudry <phil.beaudry@ibm.com>
2020-02-28 10:47:37 -0500

    Added the extra animations

--- TootsieRoll/script.js
+++ TootsieRoll/script.js
@@ -2,39 +2,61 @@
 
 $(window).scroll(function() {
    var value = $(this).scrollTop();
-   if (value > 5900) {
+   if (value > 6500){
+      $(".artwork").attr("src", "./image/wrapper-3.png");
+      document.getElementById("roll").style.display = 'none'
+   }
+   else if (value > 6000){
+      $(".artwork").attr("src", "./image/wrapper-3.png");
+      $(".roll").attr("src", "./image/roll3.png");
+      document.getElementById("roll").style.display = 'block'
+   }
+   else if (value > 5500){
+      $(".artwork").attr("src", "./image/wrapper-3.png");
+      $(".roll").attr("src", "./image/roll2.png");
+      document.getElementById("roll").style.display = 'block'
+   }
+   else if (value > 5000){
+      $(".artwork").attr("src", "./image/wrapper-3.png");
+      $(".roll").attr("src", "./image/roll1.png");
+      document.getElementById("roll").style.display = 'block'
+   }
+   else if (value > 4500) {
     $(".artwork").attr("src", "./image/wrapper-3.png");
+    $(".roll").attr("src", "./image/roll.png");
     document.getElementById("roll").style.display = 'block'
    } 
-   else if (value > 5200){
+   else if (value > 4000){
     $(".artwork").attr("src", "./image/wrapper-2.png");
+    $(".roll").attr("src", "./image/roll.png");
     document.getElementById("roll").style.display = 'block'
    } 
-   else if (value > 4600){
+   else if (value > 3500){
     $(".artwork").attr("src", "./image/wrapper-1.png");
+    $(".roll").attr("src", "./image/roll.png");
     document.getElementById("roll").style.display = 'block'
    } 
-   else if (value > 3900){
+   else if (value > 3000){
     $(".artwork").attr("src", "./image/Artwork-7.png");
     document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 3200){
+   else if (value > 2500){
     $(".artwork").attr("src", "./image/Artwork-6.png");
     document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 2700){
+   else if (value > 2000){
     $(".artwork").attr("src", "./image/Artwork-5.png");
     document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 2000){
+   else if (value > 1500){
     $(".artwork").attr("src", "./image/Artwork-4.png");
     document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 1400){
+   else if (value > 1000){
     $(".artwork").attr("src", "./image/Artwork-3.png");
     document.getElementById("roll").style.display = 'none'
    } 
-   else if (value > 700){
+   else if (value > 500){
     $(".artwork").attr("src", "./image/Artwork-2.png");
     document.getElementById("roll").style.display = 'none'
    }   
